dc107a367eaf8d8e8552ab6e19dd96378adc9646
matthew-bradow <matthew-bradow@users.noreply.github.com>
2019-09-16 15:12:53 -0600

    first-commit

---
+++ public_html/bacs350/index.php
@@ -0,0 +1,49 @@
+<?php
+
+    /*
+        Create page content by rendering a template.
+    */
+
+    $site_title = 'UNC BACS 350';
+    
+    $page_title = "Matthew's Brain";
+    
+    $content = '
+        <p>
+            <a href="..">Home</a>
+        </p>
+        <img src="images/profilePicture.jpg" style="width:200px;">
+        <p>
+            Matthew Bradow is a Junior at the University of Colorado. He is currently pursuing a Bachelor of Science degree in Software Engineering as well as a Music minor and Music Technology certificate.
+        </p>
+        <p> 
+            This page is the beginning of an ongoing project in BACS 350.
+        </p>
+        <p> 
+            It is a custom information manager.
+        </p>
+        <p> 
+            Different rooms within this PHP app will contain different types of info.
+        </p>
+        
+        <ul>
+            <li>
+                <a href="demo">Code Demos</a>
+            </li>
+            <li>
+                <a href="pattern">Design Patterns</a>
+            </li>
+            <li>
+                <a href="project">Projects</a>
+            </li>
+            <li>
+                <a href="superhero">Superhero Profiles</a>
+            </li>
+        </ul>
+    ';
+
+    include 'views.php';
+    
+    echo render_page($site_title, $page_title, $content);
+
+?>
---
+++ public_html/bacs350/superhero/index.php
@@ -0,0 +1,45 @@
+<?php
+    /*
+        Superhero Project Workshop
+    */
+
+    // Get the render_page and render_card functions
+    include 'views.php';
+
+    // Set custom settings
+    $site_title = 'UNC BACS 350';
+    $page_title = 'Superhero Gallery';
+
+    $card1 = render_card("Spider-Man", "
+        <p>Your friendly neighborhood superhero.</p>
+        <img src='https://upload.wikimedia.org/wikipedia/en/2/21/Web_of_Spider-Man_Vol_1_129-1.png' style='width:200px;'>
+        <h6>Image source: Wikipedia</h6>
+        <p>Spider-Man (Peter Parker) is a 15 year old from Queens. He was bitten by a radioactive spider and now protects New York City from the crimes he feels he has a responsibility to stop.</p>
+        ");
+    $card2 = render_card("Iron Man", "
+        <p>Genius, Billionaire, Playboy, Philanthropist.</p>
+        <img src='https://upload.wikimedia.org/wikipedia/en/e/e0/Iron_Man_bleeding_edge.jpg' style='width:200px;'>
+        <h6>Image source: Wikipedia</h6>
+        After Iron Man (Tony Stark) was kidnapped and sustained a chest injury, he built an Iron Man suit and escaped. He later continued to build more powerful and weaponized suits in order to protect himself and others.</p>
+        ");
+    $card3 = render_card("Superman", "
+        <p>Defender of Earth</p>
+        <img src='https://upload.wikimedia.org/wikipedia/en/3/35/Supermanflying.png' style='width:200px;'>
+        <h6>Image source: Wikipedia</h6>
+        <p>Superman (Clark Kent) was born on the planet Krypton. After his parents sent him to earth as a baby, he grew up on Earth and developed a plethora of powers, including flight and super strength.</p>
+        ");
+    
+
+    $content =  '
+        <h2>PROJECT #3: Superhero Gallery</h2>
+        <div class="container-fluid">
+            <div class="row">
+                ' . $card1 . $card2 . '
+                ' . $card3 . '
+            </div>
+        </div>
+    ';
+
+    // Create HTML and output the page
+    echo render_page($site_title, $page_title, $content);
+?>
