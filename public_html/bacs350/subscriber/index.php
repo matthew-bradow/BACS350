44c2d4c834edde6eabe30c297ce66061e46f01cd
matthew-bradow <matthew-bradow@users.noreply.github.com>
2019-10-10 14:16:08 -0600

    lesson 17

--- public_html/bacs350/subscriber/index.php
+++ public_html/bacs350/subscriber/index.php
@@ -1,32 +1,41 @@
-<?php 
-    // Access render_page from library
+<?php
+
+/*
+    PHP Code for more efficient CRUD
+*/
+
+
+    // Include view rendering code
     require 'views.php';
 
-    // Display the HTML with Style
-    echo render_page('UNC BACS 350', "Subscriber List", $list);
+
+    // Include all of the database code
+    require 'subscribers.php';
+
+
+    // Add explanation
+    $intro = '<h1>Lesson 19</h1>
+        <p>
+            This page was updated as part of Lesson 19 - CRUD Functions for the UNC BACS 350 class.
+        </p>';
+
+
+
+    // Add one database record
+    $name = 'Bogus Test user';
+    $email = 'bogus_user@gmail.com';
+    $subscribers = add_subscriber($db, $name, $email);
+   
+
+    // List subscriber records
+    $content = render_subscribers(list_subscribers ($db));
+    
+    
+    // Delete record to clean up bogus user
+    delete_subscriber($db, $email);
+
+
+    // Display the HTML in the page
+    echo render_page('UNC BACS 350', "Subscriber List", $intro . $content);
+
 ?>
-
-<h2>Subscriber List</h2>
-
-<p>This page demonstrates a connection to an actual database at Bluehost.</p>
-
-<h3>Step 1 - Simple page</h3>
-
-<p>
-    <b></b> Start by building and debugging a page that looks like this one.
-    Get this working first!
-</p>
-<p>
-    <a href="step1.php">Subscribers Page</a>
-</p>
-
-<h3>Step 2 - Normal page</h3>
-
-<p>
-    The simple solution will duplicate a lot of code
-    on a real website so it is not suitable for production websites. Build functions
-    for all of the key operations that should be done.
-</p>
-<p>
-    <a href="step2.php">Subscribers Page</a>
-</p>
